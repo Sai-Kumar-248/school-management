fb6806ff717584498b88e8c4aafeab84d2fae229
Sai Kumar <ksaikumar5148@gmail.com>
2025-01-09 20:34:03 +0530

    second commit

---
+++ src/main/java/com/schoolmanagement/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.schoolmanagement.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://panchatantra123.netlify.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
